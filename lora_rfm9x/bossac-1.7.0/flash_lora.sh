8f73d50a0dbc358ac544b6c106b91abc8136693d
David Fan <fand.kanade@gmail.com>
2020-03-21 21:53:26 -0400

    Added sudo to flashing lora featherwing

--- lora_rfm9x/bossac-1.7.0/flash_lora.sh
+++ lora_rfm9x/bossac-1.7.0/flash_lora.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bossac -e -w -v -R -p /dev/ttyACM0 adafruit-circuitpython-feather_m0_rfm9x-en_US-5.0.0.bin
+sudo ./bossac -e -w -v -R -p /dev/ttyACM0 adafruit-circuitpython-feather_m0_rfm9x-en_US-5.0.0.bin
